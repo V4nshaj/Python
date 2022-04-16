828a5128b3f09f843fe2d111b4ae6f2680f78f88
V4nshaj <95959007+V4nshaj@users.noreply.github.com>
2022-04-16 21:23:10 +0530

    Add files via upload
    Determine the love score between two people

---
+++ Love Calculator.py
@@ -0,0 +1,25 @@
+print("Welcome to the Love Calculator!")
+name1 = input("What is your name? \n")
+name2 = input("What is their name? \n")
+name1 = name1.lower()#to change the name in lower case
+name2 = name2.lower()#to change the name in lower case
+name=name1+name2
+t=name.count('t')
+r=name.count('r')
+u=name.count('u')
+e=name.count('e')
+l=name.count('l')
+o=name.count('o')
+v=name.count('v')
+e=name.count('e')
+s1=t+r+u+e
+print(s1)
+s2=l+o+v+e
+print(s2)
+score=int(str(s1) + str(s2))
+if score < 10 or score > 90:
+    print(f"Your score is {score}, you go together like coke and mentos.")
+elif score >= 40 and score <=50:
+    print(f"Your score is {score}, you are alright together.")
+else:
+    print(f"Your score is {score}.")
