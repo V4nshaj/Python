09510f099215acaec013f55a8a04ffeab933b7b0
V4nshaj <95959007+V4nshaj@users.noreply.github.com>
2023-07-01 07:43:33 +0530

    Update client1.py

--- Network Programming/client1.py
+++ Network Programming/client1.py
@@ -2,9 +2,9 @@
 import socket
 
 c=socket.socket()
-host=socket.gethostname()
+host=socket.gethostname()#hostname
 port=9999
-c.connect((host,port))
+c.connect((host,port))#connecting to the server
 name=input("Enter your name: ")
 c.send(bytes(name,'utf-8'))
 
