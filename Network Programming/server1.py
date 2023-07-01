f6a6258c3cd454ec3fb76cba3677c1eebecebef6
V4nshaj <95959007+V4nshaj@users.noreply.github.com>
2023-07-01 07:45:47 +0530

    Update server1.py

--- Network Programming/server1.py
+++ Network Programming/server1.py
@@ -3,15 +3,15 @@ import socket
 s=socket.socket()
 print('Socket Created')
 
-host=socket.gethostname()
-port=9999
+host=socket.gethostname()#gethostname
+port=9999#port number
 
 s.bind((host,port))
-s.listen(3)
+s.listen(3)#wait for 3 seeconds
 print('waiting for the client connection')
 
 while True:
-    c,addr=s.accept()
+    c,addr=s.accept()#accept the connection
     print(s.recv(1024).decode())
     print('connected with',addr)
     c.send(bytes('welcome','utf-8'))
