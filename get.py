de1077cd7bbaf5f5bd26ee2fa647871d6512b8be
V4nshaj <vanshajbarnwal21@gmail.com>
2024-03-14 18:23:39 +0530

    Added get function

---
+++ get.py
@@ -0,0 +1,7 @@
+people = {'john': 32, 'rob': 45, 'tim': 20}
+
+print(people.get("rob"))
+print(people['rob'])
+
+print(people.get("jim"))#this show there is none with this key
+print(people['jim'])#this shows error
