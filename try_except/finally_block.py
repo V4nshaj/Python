cec2c3a79686d840cb8bf717a52a4dbf0e906a4e
V4nshaj <vanshajbarnwal21@gmail.com>
2024-03-15 19:37:07 +0530

    final block added

---
+++ try_except/finally_block.py
@@ -0,0 +1,10 @@
+n = int(input('Enter numerator'))
+d = int(input('Enter denominator'))
+try:
+    result = n/d
+except ZeroDivisionError:#zerodivisionerror is a built in exception
+    print('Cannot divide by zero')
+else:
+    print(result)
+finally:
+    print('This part will be executed no matter what')
